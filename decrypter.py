f3a148c459c261fca06cb4eeb855fde4a6a43c0e
wesleimarcelo67 <58061353+wesleimarcelo67@users.noreply.github.com>
2025-01-31 23:18:04 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -16,7 +16,7 @@ decrypt_data = aes.decrypt(file_data)
 os.remove(file_name)
 
 ## criar o arquivo descriptografado
-new_file = "teste.txt"
+new_file = "cry.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
 new_file.close()
